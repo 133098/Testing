d7d77875a78bf7f7fdec33ab90708db2e211f15b
Zannatul Mohwa Jabin <rahmanashfakur@gmail.com>
2019-01-08 16:20:30 +0600

    ram

---
+++ src/main/java/MainApplication1.java
@@ -0,0 +1,8 @@
+/**
+ * Created by ashfakur on 2019-01-08.
+ */
+public class MainApplication1 {
+    public static void main(String[]args){
+        System.out.println("My First Programming" + " And It is in Java");
+    }
+}
